a29c8441b897002b56bb677862d2f0f3170cd3eb
PaulaAlvarezC <alvarezpaula@outlook.com>
2022-01-18 20:26:43 -0300

    Update index.js

--- src/screens/gameover/index.js
+++ src/screens/gameover/index.js
@@ -1,8 +1,9 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, Button, Image } from 'react-native';
+import { Button, Image, Text, View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+
 import Numbers from '../../components/atoms/numbers';
-import { styles } from './styles';
 import { generateRandomNumber } from '../../utils/functions';
+import { styles } from './styles';
 import { theme } from '../../utils/constants/theme';
 
 const gameOverImage = require('../../../assets/images/gameover.jpg');
@@ -15,7 +16,9 @@ const GameOverScreen = (props) => {
             {/* <Image source={{ uri: 'https://as1.ftcdn.net/v2/jpg/03/75/20/38/1000_F_375203802_FOVtD1IWxFxSRdPDlw7XtlcDJ145PV2V.jpg' }} style={styles.image} /> */}
             <Image source={gameOverImage} style={styles.image} />
             <Text style={styles.text}>Intentos: {rounds}</Text>
+            
             <Numbers number={choice} />
+            <Text style={styles.text}>Era la respuesta correcta</Text>
             <View style={styles.buttonContainer}>
                 <Button title="Nuevo Juego" onPress={() => onRestart && onRestart()} color={theme.primaryColor}/>
             </View>
